89da3f181995c31fabb3e00c611882c3113fa7a4
Antoine Sireyjol <antoine.sireyjol@gmail.com>
2020-01-30 15:48:08 +0100

    changements suite à tentatives de déploiement

--- R/run_app.R
+++ R/run_app.R
@@ -1,4 +1,10 @@
-
+#' Lancement de l'application
+#'
+#' @return lance l'application
+#'
+#' @import shiny
+#' @export
+#'
 run_app <- function() {
   shinyApp(ui = app_ui(), server = app_server)
 }
